f4839d6bb6322bb0b31af2b4955712706e8c0dc3
mpabarca <mp.abarca.alvear@gmail.com>
2025-07-30 06:26:47 +0200

    Move App.tsx to /pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./styles/global.css";
-import App from "./App.tsx";
-import ThemeLayout from "./root/ThemeLayout.tsx";
+import ThemeLayout from "./root/ThemeLayout";
+import LandingPage from "./pages/LandingPage";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ThemeLayout>
-      <App />
+      <LandingPage />
     </ThemeLayout>
   </StrictMode>
 );
